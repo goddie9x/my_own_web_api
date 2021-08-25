b747d20a29344797a03f3aaee79a9ed5c80479a1
goddie9x <goddie9x@yahoo.com>
2021-08-25 07:53:11 +0700

    re test

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,11 @@
-/* const siteRouter = require('./site'); */
+const siteRouter = require('./site');
 const scheduleRouter = require('./schedule');
 const userRouter = require('./user');
 
 function router(app) {
     app.use('/users', userRouter);
     app.use('/schedules', scheduleRouter);
-    /*  app.use('/', siteRouter); */
+    app.use('/', siteRouter);
 }
 
 module.exports = router;
