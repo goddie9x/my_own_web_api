b4cb402376c04cb0f3316285c8c1f398ed314dc1
goddie9x <goddie9x@yahoo.com>
2021-09-03 14:40:20 +0700

    update display message chat

--- src/public/scripts/Character.js
+++ src/public/scripts/Character.js
@@ -8,7 +8,7 @@ class Character {
         this.curentContainer.append(`<div class="character-${characterCount} character"></div>`);
         this.currentCharacter = $(`.character-${characterCount}`);
         this.maxWidth = this.curentContainer.width();
-        this.maxHeight = this.curentContainer.height();
+        this.maxHeight = this.curentContainer.height() - this.currentCharacter.height();
         this.setOffset();
     }
     checkAndRestoreOffsetIfCharacterOutContainer() {
@@ -31,6 +31,7 @@ class Character {
             left: this.positionX,
             top: this.positionY
         });
+        this.handlerTalkMessageDisplay();
     }
     moveLeft() {
         this.positionX -= 5;
@@ -49,12 +50,44 @@ class Character {
         this.setOffset();
     }
     talk(message) {
+        //block script or any esle code we do not expect
+        let entities = { '<': '&lt;', '>': '&gt;' };
+
+        message = message.slice(0, 540);
+        message = message.replace(
+            /[<>]/g,
+            function(s) {
+                return entities[s];
+            }
+        );
         let messageLength = message.length;
         let timeRemoveMessage = (messageLength < 30) ? (2000) : (Math.floor(messageLength / 15 * 1000));
 
         setTimeout(() => {
             this.currentCharacter.html('');
         }, timeRemoveMessage);
-        this.currentCharacter.html(`<div class="message top">${message}</div>`);
+        this.currentCharacter.html(`<div class="message ">${message}</div>`);
+        this.handlerTalkMessageDisplay();
+    }
+    handlerTalkMessageDisplay() {
+        let messageElement = this.currentCharacter.find('.message');
+        if (messageElement) {
+            let messageHeight = messageElement.height();
+            let messageWidth = messageElement.width();
+
+            //message dinamic display up or down, left or right to be comletely inside container
+            if (messageHeight > this.positionY && messageHeight + this.positionY < this.maxHeight) {
+                messageElement.addClass('bottom').removeClass('top');
+            } else {
+                messageElement.addClass('top').removeClass('bottom');
+            }
+            if (messageWidth > this.positionX && messageWidth + this.positionX < this.maxWidth) {
+                messageElement.addClass('left').removeClass('right');
+            } else {
+                messageElement.addClass('right').removeClass('left');
+            }
+        }
+
+
     }
 }
