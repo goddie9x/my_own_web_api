36f95bbc4cf20798135c1caa6674dde14654835a
goddie9x <goddie9x@yahoo.com>
2021-09-07 17:11:28 +0700

    fix name of file site controller, s with uppercase

--- src/routes/site.js
+++ src/routes/site.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const siteController = require('../app/controllers/siteController');
+const siteController = require('../app/controllers/SiteController');
 
 router.get('/notPermission', siteController.notPermission);
 router.get('/404', siteController.notFound);
